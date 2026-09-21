b718282738f89cd9db427eb34bcde95aa3f879d9
Kaina Padilla <padillakaina@gmail.com>
2026-09-21 10:27:03 +0800

    Add overtime pay for shifts over 8 hours - padilla.kaina

--- shifts.js
+++ shifts.js
@@ -3,7 +3,15 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
+  if (hours > 8) {
+    const regularPay = 8 * rate;
+    const overtimeHours = hours - 8;
+    const overtimePay = overtimeHours * rate * 1.5;
+    return Math.floor(regularPay + overtimePay);
+  }
   return Math.floor(hours * rate);
 }
 
 module.exports = { isValidShift, calculatePay };
+
+
